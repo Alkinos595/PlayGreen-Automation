b42239f3ea78507fd550c75ded00cc61aea70ba8
Alberto <foinquinosalberto@gmail.com>
2023-03-23 23:26:05 -0400

    First commit

---
+++ __test__/login.test.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const pick = require('../lib/selectors');
+const make = require('../lib/helpers');
+const config = require('../lib/config');
+const rURL = require('../lib/requestURL');
+const query = require('../lib/queries');
+
+describe('Login with phone test', () => {
+    config.set;
+    beforeAll(config.before, 60000);
+    afterAll(config.after);
+    test('Go to Page Validation', async () => {
+        const url = await make.getUrl();
+        expect(url).toMatch('https://staging.playgreen.com/');
+    }, 60000);
+
+    test('Open the login by phone form', async () => {
+        await make.wait(5000);
+        await make.clickX(page, pick.loginButton);
+        await make.clickX(page, pick.phoneButton);
+    }, 60000);
+    
+    test('Fill the form', async () => {
+        await make.click(page, pick.inputPhone);
+        await make.eraseChar(3);
+        await make.type(page, pick.inputPhone, process.env.PHONE);
+        await make.click(page, pick.submitButton);
+        await make.click(page, pick.inputOTP);
+        await make.type(page, pick.inputOTP, process.env.OTP, { delay: 100 });
+        await make.click(page, pick.buttonXnotification);
+        userID = await make.getRequestID(rURL.userID_URL);
+        console.log(`El ID del miembro es: ${userID}`);
+        user = await query.getUserById(connection, userID);
+        console.log(user);
+        await make.wait(5000);
+    }, 350000);
+});
